5ada81939bdeb54e81486bd0f87f253db38be857
thisguyhasaname <47010166+thisguyhasaname@users.noreply.github.com>
2022-06-13 21:58:44 -0500

    Stamina if elses changed to for loop

--- src/util/EnergyCheck.java
+++ src/util/EnergyCheck.java
@@ -10,32 +10,16 @@ public class EnergyCheck //extends MethodProvider
         this.methods = methods;
     }
 
-    String[] Potions = {"Stamina potion(4)", "Stamina potion(3)", "Stamina potion(2)", "Stamina potion(1)"};
+    String[] Potions = {"Stamina potion(1)", "Stamina potion(2)", "Stamina potion(3)", "Stamina potion(4)"};
 
     public void Stamina()
     {
         methods.log("in stamina checker.");
-        for (int i = 0; i < Potions.length; i++)
-        {
-            if (methods.inventory.contains("Stamina potion(1)"))
-            {
+        for (String potion : Potions) {
+            if (methods.inventory.contains(potion)) {
                 methods.log("this works");
-                methods.inventory.getItem(Potions[i]).interact();
-            }
-            else if (methods.inventory.contains("Stamina potion(2)"))
-            {
-                methods.log("this works");
-                methods.inventory.getItem(Potions[i]).interact();
-            }
-            else if (methods.inventory.contains("Stamina potion(3)"))
-            {
-                methods.log("this works");
-                methods.inventory.getItem(Potions[i]).interact();
-            }
-            else if (methods.inventory.contains("Stamina potion(4)"))
-            {
-                methods.log("this works");
-                methods.inventory.getItem(Potions[i]).interact();
+                methods.inventory.getItem(potion).interact();
+                break;
             }
         }
     }
