6c9296c69262017d92497614bafba82db26c20e4
zjmnk <zjsalerno94@gmail.com>
2022-06-14 21:37:22 -0500

    priest in peril done until step 4

---
+++ src/util/constantVariables.java
@@ -0,0 +1,19 @@
+package util;
+
+import org.osbot.rs07.api.map.Area;
+import org.osbot.rs07.api.map.Position;
+
+public class constantVariables
+{
+    public static Area westBank = new Area(3180, 3433, 3185, 3446);
+    public static Area eastBank = new Area(3250,3419,3257,3423);
+    public static Area nearRoald = new Area(3213, 3471, 3225, 3478);
+    public static Area gateToTemple = new Area(3316,3466, 3323,3470);
+    public static Area outsideTemple = new Area(3402,3484,3408,3492);
+    public static Position byTrapDoor = new Position(3405,3506,0);
+    public static Area nearGuardian = new Area(3403,9895,3407,9906);
+    public static Position guardianSafeSpot = new Position(3406,9906,0);
+    public static Area varTeleport = new Area(3207,3418,3223,3437);
+
+
+}
