4c6924ba609a8afd6f270cb64a7e39181e72b935
zsalazar94 <55766162+zsalazar94@users.noreply.github.com>
2022-06-16 08:56:05 -0500

    adding functionality to motherload mine

--- src/MotherloadMine.java
+++ src/MotherloadMine.java
@@ -10,10 +10,209 @@ import org.osbot.rs07.script.ScriptManifest;
 
 public class MotherloadMine extends Script
 {
+    Area MotherloadMine = new Area(x, y, x, y)
+    Area bankArea = new Area(x, y, x, y)
+    Area hopperArea = new Area(x, y, x, y)
+    Area sackArea = new Area(x, y, x, y)
+    Area nearRock1 = new Area(x, y, x, y)
+    Area nearRock2 = new Area(x, y, x, y)
+    Area nearRock3 = new Area(x, y, x, y)
+    Area northMiningArea = new Area(x, y, x, y)
+    Position bankPosition = new Position(x, y, z)
+    Entity rock1;
+    Entity rock2;
+    Entity rock3;
+    Entity returnRock1;
+    Entity returnRock2;
+    Entity returnRock3;
+    Entity miningRocks;
+    Area returnNearRock1 = new Area(x, y, x, y)
+    Area returnNearRock2 = new Area(x, y, x, y)
+    Area returnNearRock3 = new Area(x, y, x, y)
+    boolean isBanking = false;
+
     @Override
     public int onLoop() throws InterruptedException
     {
+        //Handles banking once inside banking area
+        if (bankArea.contains(myPosition()) && isBanking)
+            {
+                if (inventory.contains("Pay-dirt"));
+                {
+                    log("walkinging to hopperArea area");
+                    walking.webWalk(hopperArea);
+                    if (hopperArea.contains(myPosition()) && inventory.contains("Pay-dirt"))
+                    {
+                        objects.closest("Hopper").interact();
+                        sleep(random(2400,3000));
+                    }
+                }
+                else if (inventory.contains("Coal")
+                {
+                    log("walkinging to bank")
+                    walking.webWalk(bankPosition);
+                    sleep(random(1200,1800);
+                    if (bankPosition.equals(myPosition()))
+                    {
+                        if (!bank.isOpen())
+                        {
+                            log("Open bank");
+                            bank.DepositAll();
+                            sleep(random(1800,2400)
+                            if (skills.getStatic(Skill.MINING) <= 40 && !inventory.contains("Adamant pickaxe"))
+                            {
+                                bank.withdraw("Adamant pickaxe", 1);
+                                sleep(random(1800,2400);
+                            }
+                            if (skills.getStatic(Skill.MINING) > 40 && !inventory.contains("Rune pickaxe"))
+                            {
+                                bank.withdraw("Rune pickaxe", 1);
+                                sleep(random(1800,2400);
+                            }
+                            isBanking = false;
+                        }
+                    }
+                }
+                else if (!inventory.contains("Coal") && !inventory.contains("Pay-dirt"))
+                {
+                    if (!sackArea.contains(myPosition()))
+                    {
+                        log("walkinging to sack to retrieve ore");
+                        walking.webWalk(sackArea);
+                        sleep(random(1200,1800);
+                    }
+                    if (sackArea.contains(myPosition())
+                    {
+                        log("get ore from sack");
+                        objects.closest("Sack").interact();
+                        sleep(random(1800,2400);
+                    }
+                }
 
+            }
+
+        if (bankArea.contains(myPosition()) && !isBanking && !rock1.contains(myPosition())
+        {
+                log("Walking to rock1");
+
+        }
+
+        if (nearRock1.contains(myPosition()))
+        {
+            rock1 = objects.closest(new Area(x, y, x, y),id);
+            if (rock1 == null)
+            {
+                log("running past rock 1");
+                walking.webWalk(nearRock2);
+                sleep(random(1200,1600);
+            }
+            if (nearRock1.contains(myPosition()) && rock1 != null && !myPlayer().isAnimating())
+            {
+                log("Mining rock1");
+                objects.closest(rock1).interact();
+                sleep(random(1200,1600);
+            }
+        }
+
+        if (nearRock2.contains(myPosition()))
+        {
+            rock2 = objects.closest(new Area(x, y, x, y),id);
+            if (rock2 == null)
+            {
+                log("running past rock 2");
+                walking.webWalk(nearRock3);
+                sleep(random(1200,1600);
+            }
+            if (nearRock2.contains(myPosition()) && rock2 != null && !myPlayer().isAnimating())
+            {
+                log("Mining rock2");
+                objects.closest(rock2).interact();
+                sleep(random(1200,1600);
+            }
+        }
+
+        if (nearRock3.contains(myPosition()))
+        {
+            rock3 = objects.closest(new Area(x, y, x, y),id);
+            if (rock3 == null)
+            {
+                log("running past rock 3");
+                walking.webWalk(northMiningArea);
+                sleep(random(1200,1600);
+            }
+            if (nearRock3.contains(myPosition()) && rock3 != null && !myPlayer().isAnimating())
+            {
+                log("Mining rock3");
+                objects.closest(rock3).interact();
+                sleep(random(1200,1600);
+            }
+        }
+
+        if (northMiningArea.contains(myPosition()) && !inventory.isFull())
+        {
+            miningRocks = objects.closest(new Area(x, y, x, y),id); //this should be all the area i want to mine in.
+            if (miningRocks != null && !myPlayer().isAnimating())
+            {
+                log("mining pay dirt");
+                objects.closest(miningRocks).interact();
+                sleep(random(1200,1800);
+            }
+        }
+
+        if (northMiningArea.contains(myPosition()) && inventory.isFull())
+        {
+            isBanking = true;
+            walking.webWalk(returnNearRock3);
+            sleep(random(1200,1600));
+        }
+
+        if (returnNearRock3.contains(myPosition()) && isBanking)
+        {
+            returnRock3 = objects.closest(new Area(x, y, x, y),id);
+            if (returnRock3 == null)
+            {
+                log("running past rock 3");
+                walking.webWalk(returnNearRock2);
+                sleep(random(1200,1600);
+            }
+            if (nearRock3.contains(myPosition()) && returnRock3 != null && !myPlayer().isAnimating())
+            {
+                log("Mining returnRock3");
+                objects.closest(returnRock3).interact();
+                sleep(random(1200,1600);
+        }
+
+        if (returnNearRock2.contains(myPosition()) && isBanking)
+        {
+            returnRock2 = objects.closest(new Area(x, y, x, y),id);
+            if (returnRock2 == null)
+            {
+                log("running past rock 2");
+                walking.webWalk(returnNearRock1);
+                sleep(random(1200,1600);
+            }
+            if (nearRock2.contains(myPosition()) && returnRock2 != null && !myPlayer().isAnimating())
+            {
+                log("Mining returnRock2");
+                objects.closest(returnRock2).interact();
+                sleep(random(1200,1600);
+        }
+
+        if (returnNearRock1.contains(myPosition()) && isBanking)
+        {
+            returnRock1 = objects.closest(new Area(x, y, x, y),id);
+            if (returnRock1 == null)
+            {
+                log("running past rock 1");
+                walking.webWalk(bankArea);
+                sleep(random(1200,1600);
+            }
+            if (nearRock1.contains(myPosition()) && returnRock1 != null && !myPlayer().isAnimating())
+            {
+                log("Mining returnRock1");
+                objects.closest(returnRock1).interact();
+                sleep(random(1200,1600);
+        }
         return 0;
     }
 }
